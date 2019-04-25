d5c06ee46ab97a045fe6a20acc832863c7d85389
Iskra B <ibatistapoblete@gmail.com>
2019-04-25 03:22:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.scan(/[AEIOU][aeiou]/) ? true : false
+  word.scan(/\b([AEIOU]|[aeiou])?/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
